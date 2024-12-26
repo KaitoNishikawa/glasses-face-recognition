8bfc73fb826df3634405ed2a6391a74bc4809a5d
Kaito Nishikawa <kai.nishikawa808@gmail.com>
2024-12-26 14:59:52 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,106 @@
+// Import dependencies and set up http server
+const express = require("express"),
+bodyParser = require("body-parser"),
+{ urlencoded, json } = require("body-parser"),
+config = require('./config'),
+app = express().use(bodyParser.json());
+    
+
+app.post("/messaging-webhook", (req, res) => {
+    let body = req.body;
+  
+    console.log(`\u{1F7EA} Received webhook:`);
+    console.dir(body, { depth: null });
+
+    // Send a 200 OK response if this is a page webhook
+
+  if (body.object === "page") {
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+
+    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+      
+    });
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+
+// Add support for GET requests to our webhook
+app.get("/messaging-webhook", (req, res) => {
+  
+  // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+  
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Check the mode and token sent is correct
+      if (mode === "subscribe" && token === config.verifyToken) {
+        // Respond with the challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        console.log()
+        res.status(200).send(challenge);
+      } else {
+        // Respond with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
+    }
+});
+
+
+// Verify that the callback came from Facebook.
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature-256"];
+
+  if (!signature) {
+    console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
+  } else {
+    var elements = signature.split("=");
+    var signatureHash = elements[1];
+    var expectedHash = crypto
+      .createHmac("sha256", config.appSecret)
+      .update(buf)
+      .digest("hex");
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
+
+// Add this code to start the server and listen on a specific port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+console.log('hello world')
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    verifyToken: 'YOUR-VERIFY-TOKEN',
+    appSecret: 'YOUR-APP-SECRET' // if you have other configurations
+  };
