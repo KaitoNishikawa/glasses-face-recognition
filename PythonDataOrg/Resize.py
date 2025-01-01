fc6afe3b1dec88ccbf26e17268873f838596fb05
xavieryn <xavier.nishikawa@gmail.com>
2025-01-01 15:19:36 -0800

    Delete uneeded photos an adds grayscale photos for eigenfaces

--- PythonDataOrg/Resize.py
+++ PythonDataOrg/Resize.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 import os 
 import re
+import cv2
 
 directory = "Dataset"
 def is_valid_image_pillow(file_name):
@@ -18,11 +19,14 @@ for filename in os.listdir(directory):
     # Load the image as a NumPy array
     if is_valid_image_pillow(f):
 
-        image_array = np.array(Image.open(f))
+        #image_array = np.array(Image.open(f))
+        image_array = cv2.imread(f)
 
+        # Use the cvtColor() function to grayscale the image
+        gray_image = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
         # Resize the image using PIL
         new_size = (72,96)  # Desired size (width, height)
-        resized_image = Image.fromarray(image_array).resize(new_size)
+        resized_image = Image.fromarray(gray_image).resize(new_size)
 
         # Convert the resized image back to a NumPy array
         resized_array = np.array(resized_image)
