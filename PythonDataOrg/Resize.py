13647007dd8689095e68949138412cd14fc433ac
xavieryn <xavier.nishikawa@gmail.com>
2025-01-01 16:05:10 -0800

    Adds new jupyternotebook which I will finish later tonight hopefully.

--- PythonDataOrg/Resize.py
+++ PythonDataOrg/Resize.py
@@ -5,6 +5,7 @@ import re
 import cv2
 
 directory = "Dataset"
+
 def is_valid_image_pillow(file_name):
     try:
         with Image.open(file_name) as img:
@@ -15,24 +16,18 @@ def is_valid_image_pillow(file_name):
 
 for filename in os.listdir(directory): 
     f = os.path.join(directory, filename)
-    print(f)
-    # Load the image as a NumPy array
+    
     if is_valid_image_pillow(f):
-
-        #image_array = np.array(Image.open(f))
+        # Load the image
         image_array = cv2.imread(f)
-
-        # Use the cvtColor() function to grayscale the image
+        
+        # Convert to grayscale
         gray_image = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
-        # Resize the image using PIL
+        
+        # Resize the image (note: working directly with numpy array)
         new_size = (72,96)  # Desired size (width, height)
-        resized_image = Image.fromarray(gray_image).resize(new_size)
 
-        # Convert the resized image back to a NumPy array
-        resized_array = np.array(resized_image)
-
-        im = Image.fromarray(resized_array)
-        new_filename = re.sub(r'\..*$', '', filename)
-        im.save(f"formatted_dataset/{new_filename}.jpeg")
-
-print(resized_array.shape)
+        resized_array = cv2.resize(gray_image, new_size)
+    
+        new_filename = re.sub(r'\..*$', '', filename) # I love regex
+        cv2.imwrite(f"formatted_dataset/{new_filename}.jpg", resized_array)
